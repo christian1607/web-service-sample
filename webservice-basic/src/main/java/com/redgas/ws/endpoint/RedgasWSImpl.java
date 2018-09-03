93f3d264f0770728774a34e50078d670ba5788a4
Christian Altamirano Ayala <christian.altamirano.ayala@gmail.com>
2018-09-02 22:54:09 -0500

    webservice basics

---
+++ webservice-basic/src/main/java/com/redgas/ws/endpoint/RedgasWSImpl.java
@@ -0,0 +1,27 @@
+package com.redgas.ws.endpoint;
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+import com.redgas.ws.model.Product;
+
+@WebService
+public class RedgasWSImpl  {
+
+	public List<Product> listProducts() {
+		
+		return null;
+	}
+
+	public void addProduct(Product product) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void removeProducts(long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ webservice-basic/src/main/java/com/redgas/ws/model/Product.java
@@ -0,0 +1,39 @@
+package com.redgas.ws.model;
+
+import java.util.Date;
+
+public class Product {
+
+	
+	private long codigo;
+	private String nombre;
+	private double precio;
+	private Date fechaVencimiento;
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public Date getFechaVencimiento() {
+		return fechaVencimiento;
+	}
+	public void setFechaVencimiento(Date fechaVencimiento) {
+		this.fechaVencimiento = fechaVencimiento;
+	}
+	
+	
+	
+}
